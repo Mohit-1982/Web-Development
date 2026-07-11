6b59d4139b3b2677b6c2d88cdfbac8cad62011b4
Mohit <mohitdagar2k08@gmail.com>
2026-07-11 10:50:10 +0530

    add framer motion files

---
+++ FramerMotion/framerMotion/src/App.jsx
@@ -0,0 +1,43 @@
+import {motion} from 'motion/react'
+
+const App = () => {
+  return (
+    // <div className="h-screen w-full text-white bg-[#212121]">
+    //   <motion.div
+    //   className="w-32 h-32 bg-red-500 border-2 border-blue-200"
+    //   // initial={{
+    //   //   x:0,
+    //   //   y:0
+    //   // }}
+    //   drag
+    //   whileDrag={{
+    //     scale:0.8
+    //   }}
+    //   dragConstraints={{
+    //     left:0,
+    //     top:0,
+    //     right:1000,
+    //     bottom:500
+    //   }}
+    //   dragDirectionLock='true'
+    //   animate={{
+    //     // x:1000,
+    //     x:[0,800,800,0,0],
+    //     y:[0,0,400,400,0],
+    //     rotate:[0,90,180,270,360]
+    //     // rotate:500
+    //   }}
+    //   transition={{
+    //     duration:2,
+    //     delay:1,
+    //     // repeat:Infinity,
+    //     ease:'anticipate'
+    //   }}
+    //   ></motion.div>
+    // </div>
+
+    <h1 className=''>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur officiis magni, saepe sint sunt harum quae pariatur nihil sit quia temporibus placeat esse repellendus recusandae! Aperiam repellendus modi molestias eos vel accusamus laboriosam, iure, facilis, quos similique distinctio vero quae dolorum amet dolor quis consequuntur esse nobis perferendis eveniet accusantium hic! Eaque magni, culpa molestias aliquid modi reiciendis placeat rerum repudiandae! Quo enim nostrum commodi, natus sapiente aliquid blanditiis. Et fugit ea mollitia ipsum possimus, cum corrupti placeat suscipit animi autem quis, ducimus nostrum, quasi reiciendis nulla ad maiores excepturi! Debitis iure obcaecati sit blanditiis? Eligendi commodi ratione eaque repellat? <br /></h1>
+  )
+}
+
+export default App
